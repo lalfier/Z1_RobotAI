837dd07b05d2b7fd02df8f1c20090e0634be3dc4
lalfier <lalfier@gmail.com>
2021-08-31 15:36:27 +0200

    Added - Camera manager to change camera size, whole scene is visible on different screen aspects.

---
+++ Assets/Scripts/CameraManager.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    [Tooltip("Starting camera aspect, in this case 16/9 (width/height)")]
+    public float defaultAspect = 1.777778f;
+    [Tooltip("Starting camera size, in this case 5")]
+    public float defaultSize = 5f;
+
+    private void Awake()
+    {
+        Camera camera = GetComponent<Camera>();
+        if(camera.aspect <= defaultAspect)
+        {
+            // On application start set bigger camera size for smaller aspect. Whole scene must be visible.
+            camera.orthographicSize = 1 / camera.aspect * defaultAspect * defaultSize;
+        }
+    }
+}
